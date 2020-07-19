541273b313e6019aaa56015637e68f7f887160d2
Wepn <445291992@qq.com>
2020-07-20 03:15:26 +0800

    delete

--- wepn/python study/spider_leeson_1.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-res = requests.get('https://localprod.pandateacher.com/python-manuscript/crawler-html/spider-men5.0.html')
-
-a = open('D:/Users/Wepn/PycharmProjects/wepn/html_code.txt', 'a+', encoding='utf-8')
-a.write(res.text)
-a.close()
-
